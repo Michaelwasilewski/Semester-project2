4a2751093d14974fbb80b2570005bca59911f8b9
Michael Wasilewski <michael_grimestad94@hotmail.com>
2022-11-16 11:17:39 +0100

    add vite,tailwind,postcss,autoprefixer, prettier

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// const { resolve } = require("path");
+
+export default {
+  build: {
+    // rollupOptions: {
+    //   input: {
+    //     home: resolve(__dirname, "index.html"),
+    //     signUp: resolve(__dirname, "signup.html"),
+    //     logIn: resolve(__dirname, "login.html"),
+    //     welcome: resolve(__dirname, "welcome.html"),
+    //     createPost: resolve(__dirname, "create-post.html"),
+    //     singlePost: resolve(__dirname, "single-post.html"),
+    //     editPost: resolve(__dirname, "edit-post.html"),
+    //   },
+    // },
+  },
+};
