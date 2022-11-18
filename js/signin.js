4401f258d644ad09a1f5bfaef2aa1b13850142cb
Michael Wasilewski <michael_grimestad94@hotmail.com>
2022-11-18 14:29:18 +0100

    fix code

--- js/signin.js
+++ js/signin.js
@@ -16,75 +16,71 @@ console.log(passwordError);
 const errorMessage = document.querySelector('#general-error');
 
 if (logInForm) {
-    logInForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      let isEmail = false;
-      if (email.value.trim().length > 0) {
-        emailError.classList.add('hidden');
-        isEmail = true;
-      } else {
-        emailError.classList.remove('hidden');
-      }
-  
-      let isValidEmail = false;
-      if (email.value.trim().length && validateEmail(email.value) === true) {
-        emailErrorNotValid.classList.add('hidden');
-        isValidEmail = true;
-      } else if (
-        email.value.trim().length &&
-        validateEmail(email.value) !== true
-      ) {
-        emailErrorNotValid.classList.remove('hidden');
-      }
-  
-      let isPassword = false;
-  
-      if (password.value.trim().length >= 8) {
-        passwordError.classList.add('hidden');
-        isPassword = true;
-      } else {
-        passwordError.classList.remove('hidden');
-      }
-  
-      let isFormValid = isEmail && isValidEmail && isPassword;
-  
-      if (isFormValid) {
-        const userData = {
-          email: email.value,
-          password: password.value,
-        };
-  
-        const LOGIN_USER_URL_ENDPOINT = `${USER_LOGIN_URL}`;
-  
-        (async function logInUser() {
-          const response = await fetch(LOGIN_USER_URL_ENDPOINT, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-          });
-          if (response.ok) {
-            const data = await response.json();
-            saveToken(data.accessToken);
-            const userToSave = {
-              name: data.name,
-              email: data.email,
-            };
-            saveUser(userToSave);
-            location.href = '/home.html';
-          } else {
-            const err = await response.json();
-            const message = `An Error occured: ${err.message}`;
-            throw new Error(message);
-          }
-        })().catch((err) => {
-          errorMessage.innerHTML = ` Sorry ${err.message}`;
+  logInForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let isEmail = false;
+    if (email.value.trim().length > 0) {
+      emailError.classList.add('hidden');
+      isEmail = true;
+    } else {
+      emailError.classList.remove('hidden');
+    }
+
+    let isValidEmail = false;
+    if (email.value.trim().length && validateEmail(email.value) === true) {
+      emailErrorNotValid.classList.add('hidden');
+      isValidEmail = true;
+    } else if (email.value.trim().length && validateEmail(email.value) !== true) {
+      emailErrorNotValid.classList.remove('hidden');
+    }
+
+    let isPassword = false;
+
+    if (password.value.trim().length >= 8) {
+      passwordError.classList.add('hidden');
+      isPassword = true;
+    } else {
+      passwordError.classList.remove('hidden');
+    }
+
+    let isFormValid = isEmail && isValidEmail && isPassword;
+
+    if (isFormValid) {
+      const userData = {
+        email: email.value,
+        password: password.value,
+      };
+
+      const LOGIN_USER_URL_ENDPOINT = `${USER_LOGIN_URL}`;
+
+      (async function logInUser() {
+        const response = await fetch(LOGIN_USER_URL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
         });
-      } else {
-        console.log('Validation Failed');
-      }
-    });
-  }
-  
+        if (response.ok) {
+          const data = await response.json();
+          saveToken(data.accessToken);
+          const userToSave = {
+            name: data.name,
+            email: data.email,
+          };
+          saveUser(userToSave);
+          location.href = '/home.html';
+        } else {
+          const err = await response.json();
+          const message = `An Error occured: ${err.message}`;
+          throw new Error(message);
+        }
+      })().catch((err) => {
+        errorMessage.innerHTML = ` Sorry ${err.message}`;
+      });
+    } else {
+      console.log('Validation Failed');
+    }
+  });
+}
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ export default {
   build: {
     rollupOptions: {
       input: {
-        home: resolve(__dirname, "index.html"),
-        signUp: resolve(__dirname, "signin.html"),
-        logIn: resolve(__dirname, "login.html"),
-        welcome: resolve(__dirname, "welcome.html"),
-        createPost: resolve(__dirname, "create-post.html"),
-        singlePost: resolve(__dirname, "single-post.html"),
-        editPost: resolve(__dirname, "edit-post.html"),
+        home: resolve(__dirname, 'index.html'),
+        signUp: resolve(__dirname, 'signin.html'),
+        logIn: resolve(__dirname, 'login.html'),
+        welcome: resolve(__dirname, 'welcome.html'),
+        createPost: resolve(__dirname, 'create-post.html'),
+        singlePost: resolve(__dirname, 'single-post.html'),
+        editPost: resolve(__dirname, 'edit-post.html'),
       },
     },
   },
