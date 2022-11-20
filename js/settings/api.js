fc7d71362d563fa75f64aa6350803efaf534f8f7
Michael Wasilewski <michael_grimestad94@hotmail.com>
2022-11-20 22:16:02 +0100

    fetch all listings to homepage, add new endpoint to api.js

--- js/settings/api.js
+++ js/settings/api.js
@@ -6,5 +6,7 @@ const API_BASE_URL = 'https://api.noroff.dev/';
 // AUTH
 const USER_LOGIN_URL = API_BASE_URL + 'api/v1/auction/auth/login';
 const USER_SIGNUP_URL = API_BASE_URL + 'api/v1/auction/auth/register';
-export { API_BASE_URL, USER_LOGIN_URL, USER_SIGNUP_URL };
+// LISTINGS
+const GET_LISTINGS_BY_ID_URL = API_BASE_URL + 'api/v1/auction/listings';
+export { API_BASE_URL, USER_LOGIN_URL, USER_SIGNUP_URL, GET_LISTINGS_BY_ID_URL };
 // https://api.noroff.dev/api/v1/social/auth/register
