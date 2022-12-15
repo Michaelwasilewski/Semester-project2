1d656be8641cb7e90fcc32e103c24b4a6845acf8
Michael Wasilewski <michael_grimestad94@hotmail.com>
2022-12-15 12:06:03 +0100

    Update listingsById.js

--- js/listingsById.js
+++ js/listingsById.js
@@ -129,11 +129,11 @@ async function getListingsById() {
     return listOfBiddings;
   }
   listingDetails.innerHTML = `
-  <div class=" bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+  <div class="border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
   <div>
     <img class="rounded-t-lg h-full w-full object-cover" src="${media}" alt="" />
   </div>
-  <div class="p-4">
+  <div class="p-4 bg-gray-800">
     <a href="#">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-orange-300">${title}</h5>
     </a>
