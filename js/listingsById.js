1fe01db5c766c9cf6f23976ea26af8165c944963
Michael Wasilewski <michael_grimestad94@hotmail.com>
2022-12-15 12:06:48 +0100

    Update listingsById.js

--- js/listingsById.js
+++ js/listingsById.js
@@ -135,7 +135,7 @@ async function getListingsById() {
   </div>
   <div class="p-4 bg-gray-800">
     <a href="#">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-orange-300">${title}</h5>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-orange-300">${title}</h5>
     </a>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${description}</p>
     <span
